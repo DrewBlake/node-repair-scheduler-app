901c6243ab3fce10ab420c00f2218b5d15ee8bf4
Andrew Blake <drewblake1974@gmail.com>
2018-09-06 21:43:33 -0400

    update ajax functions, error handling

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
 
 const USER_SEARCH_URL = '/api/users'; 
+let token;
 
-function getDataFromUserAPI (callback) {
+function getDataFromUserAPI(callback) {
 	console.log(USER_SEARCH_URL);
 	const settings = {
 		url: USER_SEARCH_URL,
@@ -12,7 +13,7 @@ function getDataFromUserAPI (callback) {
 	$.ajax(settings);
 }
 
-function displayUserData (data) {
+function displayUserData(data) {
 	console.log(data);
 	console.log('hello1');
 	let repairList = '';
@@ -24,14 +25,31 @@ function displayUserData (data) {
 						Repair History <br>Contact Info: ${data[j].contactInfo}</br></ul>`;
 		for (let i = 0; i < data[j].repairInfo.length; i++) {
 			repairList += `<li>${data[j].repairInfo[i].description}  
-							Date: ${data[j].repairInfo[i].date}</li>`;	
+							Date: ${moment(data[j].repairInfo[i].date).format('MMM Do YYYY')}</li>`;	
 		}
 		$('.js-user-list').html(`${repairList}`);		
 	}
 	repairList = '';
 }
 
-function renderForm() {
+function handleLogin(data) {
+	token = data.authToken;
+	console.log(token);
+	$('#js-user-in-form').hide();
+	$('.js-back').hide();
+	$('.js-search-all-users').hide();
+
+	//if()
+}
+
+function displaySignUpData(data) {
+	let customerInfo = `Sign up successful! Your user name is: ${data.username}
+		and your contact info is: ${data.contactInfo}`;
+
+	$('.js-sign-up-list').html(customerInfo);
+}
+
+function renderSignUpForm() {
 	return `
 		<form action='#' class="js-sign-up-form">
 			<fieldset name="contact-info">
@@ -61,8 +79,27 @@ function renderForm() {
 	    </form>`;
 }
 
-function displayForm() {
-	$('#js-user-form').html(renderForm);
+function renderSignInForm() {
+	return `
+		<form action='#' class="js-sign-in-form">
+			<fieldset name="User Info">
+		    	<legend>User Info</legend>
+		    	<label for="usernameIn">Username</label>
+		        <input type="text" id="usernameIn" placeholder="Username" required>
+		        <br>
+		        <label for="passwordIn">Password</label>
+		        <input type="text" id="passwordIn" placeholder="Password" required>
+		    </fieldset>
+		    <button type="submit">Submit</button>
+		</form>`;
+}
+
+function displaySignInForm() {
+	$('#js-user-in-form').html(renderSignInForm);
+}
+
+function displaySignUpForm() {
+	$('#js-user-form').html(renderSignUpForm);
 }
 
 function postDataToUserApi(data, callback) {
@@ -75,6 +112,22 @@ function postDataToUserApi(data, callback) {
 		success: callback
 	}
 	$.ajax(settings);
+}
+
+function postDataToLoginApi(data, callback) {
+	$('.js-sign-in-error').hide();
+	const settings = {
+		url: 'api/auth/login',
+		type: 'POST',
+		data: JSON.stringify(data),
+		dataType: 'json',
+		contentType: 'application/json',
+		success: callback,	
+	}
+	$.ajax(settings).fail(function() {
+		$('.js-sign-in-error').html('<h2>Incorrect username or password</h2>');
+		$('.js-sign-in-error').show();
+	});
 }
 
 function handleSubmitButtonClick() {
@@ -103,23 +156,47 @@ function handleSubmitButtonClick() {
 			}
 		}
 		console.log(userData);
-		postDataToUserApi(userData, displayUserData);
+		postDataToUserApi(userData, displaySignUpData);
 		$('.js-user-list').show();
 		$('.js-sign-up-form').hide();
 		$('.js-sign-up').show();
 	});
 }
 
-function handleBackButtonClick () {
+function handleSubmitInButtonClick() {
+	$('.js-sign-in-form').submit(function(event) {
+		event.preventDefault();
+		const usernameIn = $('#usernameIn').val();
+		const passwordIn = $('#passwordIn').val();
+		$('#usernameIn').val('');
+		$('#passwordIn').val('');
+		console.log(usernameIn);
+		console.log(passwordIn);
+		
+		const userData = {
+			username: usernameIn,
+			password: passwordIn
+		}
+		postDataToLoginApi(userData, handleLogin);
+
+	});
+}
+
+function handleBackButtonClick() {
 	$('.js-back').on('click', function(event) {
 		console.log('back pressed');
 		$('.js-user-list').hide();
+		$('#js-user-form').hide();
+		$('#js-user-in-form').hide();
 		$('.js-back').hide();
+		$('.js-sign-up').show();
+		$('.js-sign-in').show();
+		$('.js-sign-in-error').hide();
 		$('.js-search-all-users').show();
 	});
 }
 
-function handleSearchUsersButton () {
+function handleSearchUsersButton() {
 	$('.js-search-all-users').on('click', function(event) {
 		console.log('hello2');
 		getDataFromUserAPI(displayUserData);
@@ -129,18 +206,34 @@ function handleSearchUsersButton () {
 	});
 }
 
-function handleSignUpButtonClick () {
+function handleSignInButtonClick() {
+	$('.js-sign-in').on('click', function(event) {
+		$('.js-sign-in').hide();
+		$('.js-sign-up').hide();
+		console.log('sign in click');
+		$('.js-back').show();
+		$('#js-user-in-form').show();
+		displaySignInForm();
+		handleSubmitInButtonClick();
+	});
+}
+
+function handleSignUpButtonClick() {
 	$('.js-sign-up').on('click', function(event) {
 		console.log('sign up click');
 		$('.js-sign-up').hide();
-		displayForm();
+		$('.js-sign-in').hide();
+		$('.js-back').show();
+		$('#js-user-form').show();
+		displaySignUpForm();
 		handleSubmitButtonClick();
 	});
 }
 
-function runApp () {
+function runApp() {
 	$('.js-back').hide();
 	//$('.js-search-all-users').hide();
+	handleSignInButtonClick();
 	handleSignUpButtonClick();
 	handleSearchUsersButton();
 	handleBackButtonClick();
